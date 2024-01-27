15db68dceae83e9076cd6d2a7f579211fe20538a
Ratnesh Gupta <ratneshkrgupt@gmail.com>
2024-01-27 12:11:33 +0530

    Implemented tasklist

--- database/migrations/2024_01_27_042841_create_tasks_table.php
+++ database/migrations/2024_01_27_042841_create_tasks_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('project_id')->nullable()->constrained();
+
+            $table->integer('priority');
+            $table->string('title');
+            $table->text('description')->nullable();
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -22,6 +30,14 @@ return new class extends Migration
      */
     public function down(): void
     {
+        // drop existing foreign keys
+        Schema::table('tasks', function (Blueprint $table) {
+            if (Schema::hasColumn('tasks', 'project_id')) {
+                $table->dropForeign(['project_id']);
+            }
+        });
+
+        // drop the table
         Schema::dropIfExists('tasks');
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => '/tasks', 'as' => 'tasks.'], function () {
+    Route::get('/', [TaskController::class, 'list']);
+
+    Route::get('/{id}', [TaskController::class, 'get'])->where('id', '[1-9][0-9]*');
+
+    Route::post('/', [TaskController::class, 'store']);
+    
+    Route::put('/{id}', [TaskController::class, 'update'])->where('id', '[1-9][0-9]*');
+
+    Route::delete('/{id}', [TaskController::class, 'delete'])->where('id', '[1-9][0-9]*');
+    
+    Route::put('/', [TaskController::class, 'reorder']);
 });
